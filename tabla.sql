27749551434884acd978e969523710c86c6d2ea1
BenjaminGuV <benjaminguv001@hotmail.com>
2012-02-02 09:29:28 -0600

    tabla usuarios

---
+++ tabla.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(25) DEFAULT NULL,
+  `pass` varchar(40) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
+
