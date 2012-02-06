cdac2e75797593a7767b0ffec462167108f54b24
BenjaminGuV <benjaminguv001@hotmail.com>
2012-02-06 10:16:29 -0600

    Cambiamos los valores del pass

--- tabla.sql
+++ tabla.sql
@@ -1,8 +1,7 @@
-CREATE TABLE `usuarios` (
+CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(25) DEFAULT NULL,
-  `pass` varchar(40) DEFAULT NULL,
+  `pass` varchar(64) DEFAULT NULL,
   `email` varchar(80) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
-
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
